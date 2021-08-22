28416707383263f5eaae5cf96c7dd6178e13ffbe
Nisa07 <escalonanicolle@gmail.com>
2021-08-22 17:50:58 -0500

    Cambios

--- above/index.js
+++ above/index.js
@@ -1,15 +1,20 @@
-function textoAbout(){
-    const parrafo =getElementById("parrafo")
-    const userAction = async () => {
-        const url='http://localhost:3000/mispublicaciones'
-        const res = await fetch(url, {
-          method: "GET", 
-          headers: {
-            "Content-Type": "application/json;charset=UTF-8"
-          }
-        
-        });  return res
-    }
-    const res=userAction();
-    parrafo.append(`${res}`)
-  }
+async function renderData() {
+    let parrafo= document.getElementById("parrafo");
+    let res = await fetch("http://localhost:3000/mispublicaciones");
+    let resultaData = await res.json();
+    parrafo.append(resultaData[0].texto);
+
+    
+}
+
+renderData();
+
+const btnCrear = document.getElementById('btnCrear');
+const modalIngreso = new bootstrap.Modal(document.getElementById('modalIngreso'))// Lo jala por id
+let opcion = ''
+
+btnCrear.addEventListener('click',()=>{
+
+    modalIngreso.show() // abre el boton
+    opcion ='crear'
+})
